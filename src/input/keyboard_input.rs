791edc9719bbe48776f6a157584bc71e5bee46bf
Alireza Zabet <ahzabet23@gmail.com>
2024-03-26 21:37:23 +0400

    Add light

--- src/input/keyboard_input.rs
+++ src/input/keyboard_input.rs
@@ -7,6 +7,15 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         ..default()
     },));
 
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(8.550, 8.6, 8.685),
+        ..default()
+    });
+
     commands.spawn(SceneBundle {
         scene: asset_server.load("models/keyboard/keyboard_final_raw.glb#Scene0"),
         ..default()
