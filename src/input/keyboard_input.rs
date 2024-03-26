722dc328382e0b7d3cfb7410978fc466446e0e47
Alireza Zabet <ahzabet23@gmail.com>
2024-03-26 21:45:44 +0400

    Change background color

--- src/input/keyboard_input.rs
+++ src/input/keyboard_input.rs
@@ -1,12 +1,14 @@
 use bevy::{input::keyboard::KeyboardInput, prelude::*};
 
 pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Fix the camera positioning later
     commands.spawn((Camera3dBundle {
         transform: Transform::from_xyz(0.550, 0.6, 0.685)
             .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
         ..default()
     },));
 
+    // Fix the light positioning later
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
             shadows_enabled: true,
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use input::keyboard_input::{print_keyboard_event_system, setup};
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::rgb(0.46, 0.46, 0.46)))
         .add_systems(Startup, setup)
         .add_systems(Update, print_keyboard_event_system)
         .run();
