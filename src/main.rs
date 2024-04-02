7b8eed1646e11e7d4e16a960efd36a3d7069d29d
Alireza Zabet <ahzabet23@gmail.com>
2024-04-02 23:41:44 +0400

    Remove debugging code from `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod input;
 mod setup;
 
 use bevy::prelude::*;
-use input::keyboard_input::{handle_keyboard_event, print_keyboard_event};
+use input::keyboard_input::handle_keyboard_event;
 use setup::setup::{spawn_camera, spawn_keyboard, spawn_light};
 
 fn main() {
@@ -11,6 +11,5 @@ fn main() {
         .insert_resource(ClearColor(Color::rgb(0.46, 0.46, 0.46))) // To have the canvas blend with our terminal
         .add_systems(Startup, (spawn_camera, spawn_light, spawn_keyboard))
         .add_systems(Update, handle_keyboard_event)
-        .add_systems(Update, print_keyboard_event) // For debugging purposes. Remove later.
         .run();
 }
