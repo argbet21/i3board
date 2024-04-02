2373526b18b9cdcdf466f76c5cab55c2be23f096
Alireza Zabet <ahzabet23@gmail.com>
2024-04-02 23:45:38 +0400

    Overhaul keyboard event handling to reduce complexity and duplication
    - Integrate `KeyCodeWrapper` and `Keys` to establish a unified key mapping system, aligning Blender-named keys with Rust `KeyCode`'s.
    - Replace manual key checks with a dynamic, hashmap-based approach, allowing for iteration over a contiguous range of keys (0..104). This significantly simplifies the logic by moving away from handling keys in non-contiguous ranges, which previously led to increased complexity and code duplication.
    - The new system enhances code readability and maintainability by utilizing a single, straightforward loop for key press and release actions, adhering to the DRY principle.
    
    This revision significantly improves how keyboard inputs are processed, setting a solid foundation for future enhancements and maintenance.

--- src/input/keyboard_input.rs
+++ src/input/keyboard_input.rs
@@ -1,13 +1,18 @@
-use bevy::{input::keyboard::KeyboardInput, prelude::*};
+use bevy::prelude::*;
+use std::collections::HashMap;
 
-pub fn print_keyboard_event(mut keyboard_input_events: EventReader<KeyboardInput>) {
-    for event in keyboard_input_events.read() {
-        info!("{:?}", event);
-    }
-}
+use super::parse::{KeyCodeWrapper, Keys};
 
-// This function performs 3 crucial tasks
-// - It checks which key was pressed
+// This function performs 4 crucial tasks.
+// - Parsing:
+//   Keys have predetermined values in Blender. This function maps those to sequential ID's in Rust.
+//   We do this to get a range (0..104) of keys that we can iterate over.
+//   The alternative to a single contiguous range is a set of non-contiguous ranges (e.g., { 0..=9, a..=z, ..etc }).
+//   This causes a number of issues as it adds:
+//      1. Complexity: How would an implementation of keys that do not implement the `Range` trait look like? (e.g., f1..=f12, arrow keys, ..etc).
+//      2. Code Duplication: The core logic for every key is the same. Having multiple ranges violates the DRY principle.
+//
+// - It iterates through a built in `KeyCode` range and checks which key was pressed.
 // - It translates it accordingly
 // - It plays a sound effect accordingly
 pub fn handle_keyboard_event(
@@ -16,127 +21,50 @@ pub fn handle_keyboard_event(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
 ) {
-    // Press'n'Release start
-    // An internal function to see if you *really* pressed a key IRL.
-    if key.just_pressed(KeyCode::Digit0) {
-        // And if so, iterate through all the entities in our scene
-        for (name, mut transform) in query.iter_mut() {
-            // And translate only the entity named "0" (downwards)
-            // since we *really* only pressed the "0" key on our keyboard,
-            // otherwise all entities would move, i.e: the whole keyboard.
-            // Note: this assumes we've properly named your entities in Blender
-            if name.as_str() == "0" {
-                transform.translation.y -= 0.05;
+    let mut hashmap: HashMap<u8, String> = HashMap::with_capacity(104);
+    for (index, key) in Keys::into_iter().enumerate() {
+        hashmap.insert(index as u8, String::from(key));
+    }
 
-                commands.spawn(AudioBundle {
-                    source: asset_server.load("audio/sfx/keyboard_pressed/key.ogg"),
-                    ..default()
-                });
+    for (index, keycode) in KeyCodeWrapper::into_iter().enumerate() {
+        // An internal function to see if you *really* pressed a key IRL.
+        if key.just_pressed(keycode) {
+            let pressed_key = hashmap.get(&(index as u8));
+
+            // And if so, iterate through all the keys in our scene
+            for (name, mut transform) in query.iter_mut() {
+                // And translate only the `pressed_key` (downwards)
+                // since we *really* only pressed the `pressed_key` key on our keyboard,
+                // otherwise all entities would move, i.e: the whole keyboard.
+                if name.as_str() == pressed_key.unwrap().as_str() {
+                    transform.translation.y -= 0.05;
+
+                    commands.spawn(AudioBundle {
+                        source: asset_server.load("audio/sfx/pressed/key.ogg"),
+                        ..default()
+                    });
+                }
+            }
+        }
+
+        // An internal function to see if you *really* released a key IRL.
+        if key.just_released(keycode) {
+            let released_key = hashmap.get(&(index as u8));
+
+            // And if so, iterate through all the keys in our scene.
+            for (name, mut transform) in query.iter_mut() {
+                // And translate only the `pressed_key` (upwards)
+                // since we *really* only pressed the `pressed_key` key on our keyboard,
+                // otherwise all entities would move, i.e: the whole keyboard.
+                if name.as_str() == released_key.unwrap().as_str() {
+                    transform.translation.y += 0.05;
+
+                    commands.spawn(AudioBundle {
+                        source: asset_server.load("audio/sfx/released/key.ogg"),
+                        ..default()
+                    });
+                }
             }
         }
     }
-
-    // An internal function to see if you *really* released a key IRL.
-    if key.just_released(KeyCode::Digit0) {
-        // And if so, iterate through all the entities in our scene.
-        for (name, mut transform) in query.iter_mut() {
-            // And translate only the entity named "0" (upwards)
-            // since we *really* only pressed the "0" key on our keyboard,
-            // otherwise all entities would move, i.e: the whole keyboard.
-            // Note: this assumes we've properly named your entities in Blender
-            if name.as_str() == "0" {
-                transform.translation.y += 0.05;
-
-                commands.spawn(AudioBundle {
-                    source: asset_server.load("audio/sfx/keyboard_released/key.ogg"),
-                    ..default()
-                });
-            }
-        }
-    }
-    // Press'n'Release end
-
-    // Press'n'Release start
-    if key.just_pressed(KeyCode::Backspace) {
-        for (name, mut transform) in query.iter_mut() {
-            if name.as_str() == "backspace" {
-                transform.translation.y -= 0.05;
-
-                commands.spawn(AudioBundle {
-                    source: asset_server.load("audio/sfx/keyboard_pressed/backspace.ogg"),
-                    ..default()
-                });
-            }
-        }
-    }
-
-    if key.just_released(KeyCode::Backspace) {
-        for (name, mut transform) in query.iter_mut() {
-            if name.as_str() == "backspace" {
-                transform.translation.y += 0.05;
-
-                commands.spawn(AudioBundle {
-                    source: asset_server.load("audio/sfx/keyboard_released/backspace.ogg"),
-                    ..default()
-                });
-            }
-        }
-    }
-    // Press'n'Release end
-
-    // Press'n'Release start
-    if key.just_pressed(KeyCode::Enter) {
-        for (name, mut transform) in query.iter_mut() {
-            if name.as_str() == "enter" {
-                transform.translation.y -= 0.05;
-
-                commands.spawn(AudioBundle {
-                    source: asset_server.load("audio/sfx/keyboard_pressed/enter.ogg"),
-                    ..default()
-                });
-            }
-        }
-    }
-
-    if key.just_released(KeyCode::Enter) {
-        for (name, mut transform) in query.iter_mut() {
-            if name.as_str() == "enter" {
-                transform.translation.y += 0.05;
-
-                commands.spawn(AudioBundle {
-                    source: asset_server.load("audio/sfx/keyboard_released/enter.ogg"),
-                    ..default()
-                });
-            }
-        }
-    }
-    // Press'n'Release end
-
-    // Press'n'Release start
-    if key.just_pressed(KeyCode::Space) {
-        for (name, mut transform) in query.iter_mut() {
-            if name.as_str() == "spacebar" {
-                transform.translation.y -= 0.05;
-
-                commands.spawn(AudioBundle {
-                    source: asset_server.load("audio/sfx/keyboard_pressed/spacebar.ogg"),
-                    ..default()
-                });
-            }
-        }
-    }
-
-    if key.just_released(KeyCode::Space) {
-        for (name, mut transform) in query.iter_mut() {
-            if name.as_str() == "spacebar" {
-                transform.translation.y += 0.05;
-
-                commands.spawn(AudioBundle {
-                    source: asset_server.load("audio/sfx/keyboard_released/spacebar.ogg"),
-                    ..default()
-                });
-            }
-        }
-    }
-    // Press'n'Release end
 }
