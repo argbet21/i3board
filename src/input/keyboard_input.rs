4cc124b24439ea18e5d8ee551fbd15c0e261e635
Alireza Zabet <ahzabet23@gmail.com>
2024-04-05 14:08:01 +0400

    Optimize entity querying in keyboard event handling
    - Refactor `handle_keyboard_event` function to query entities using `&Key` instead of `&Name`. This change narrows the query scope to specifically target key entities, enhancing the efficiency of event processing by excluding irrelevant entities from the scene.
    - Update import paths to reflect the relocation of `Key`, `KeyCodeWrapper`, and `Keys` into the `crate::parser::parser` module, streamlining the project structure and improving code readability.
    
    This adjustment improves the performance of keyboard event handling by ensuring that only entities representing keys are considered during event processing, reducing computational overhead and focusing on entities of interest.

--- src/input/keyboard_input.rs
+++ src/input/keyboard_input.rs
@@ -1,7 +1,6 @@
+use crate::parser::parser::{Key, KeyCodeWrapper, Keys};
 use bevy::prelude::*;
 use std::collections::HashMap;
-
-use super::parse::{KeyCodeWrapper, Keys};
 
 // This function performs 4 crucial tasks.
 // - Parsing:
@@ -12,12 +11,13 @@ use super::parse::{KeyCodeWrapper, Keys};
 //      1. Complexity: How would an implementation of keys that do not implement the `Range` trait look like? (e.g., f1..=f12, arrow keys, ..etc).
 //      2. Code Duplication: The core logic for every key is the same. Having multiple ranges violates the DRY principle.
 //
-// - It iterates through a built in `KeyCode` range and checks which key was pressed.
+// - It iterates through a built-in `KeyCode` range and checks which key was pressed.
 // - It translates it accordingly
 // - It plays a sound effect accordingly
 pub fn handle_keyboard_event(
+    // Get immutable access to the `ButtonInput` resource. This resource describes a "press-able" input of type `T`.
     key: Res<ButtonInput<KeyCode>>,
-    mut query: Query<(&Name, &mut Transform)>,
+    mut query: Query<(&Key, &mut Transform)>,
     mut commands: Commands,
     asset_server: Res<AssetServer>,
 ) {
@@ -26,37 +26,41 @@ pub fn handle_keyboard_event(
         hashmap.insert(index as u8, String::from(key));
     }
 
+    // Iterate over all `KeyCode`'s, checking whether any has been pressed and/or released.
     for (index, keycode) in KeyCodeWrapper::into_iter().enumerate() {
-        // An internal function to see if you *really* pressed a key IRL.
+        // `ButtonInput::just_pressed` will return true one frame after a press event if `input` has just been pressed.
         if key.just_pressed(keycode) {
+            // Get the entity corresponding to the pressed `KeyCode`.
+            // Note that this works because the iterators - `KeyCodeWrapper::into_iter()` and `Keys::into_iter()` are in-sync.
+            // See `crate::parser::parser.rs` for more details.
             let pressed_key = hashmap.get(&(index as u8));
 
             // And if so, iterate through all the keys in our scene
-            for (name, mut transform) in query.iter_mut() {
+            for (key, mut transform) in query.iter_mut() {
                 // And translate only the `pressed_key` (downwards)
                 // since we *really* only pressed the `pressed_key` key on our keyboard,
                 // otherwise all entities would move, i.e: the whole keyboard.
-                if name.as_str() == pressed_key.unwrap().as_str() {
+                if key.name == pressed_key.unwrap().as_str() {
                     transform.translation.y -= 0.05;
 
                     commands.spawn(AudioBundle {
-                        source: asset_server.load("audio/sfx/pressed/key.ogg"),
+                        source: asset_server.load("audio/sfx/released/key.ogg"),
                         ..default()
                     });
                 }
             }
         }
 
-        // An internal function to see if you *really* released a key IRL.
+        // `ButtonInput::just_released` returns true one frame after a release event if `input` has just been released.
         if key.just_released(keycode) {
             let released_key = hashmap.get(&(index as u8));
 
             // And if so, iterate through all the keys in our scene.
-            for (name, mut transform) in query.iter_mut() {
+            for (key, mut transform) in query.iter_mut() {
                 // And translate only the `pressed_key` (upwards)
                 // since we *really* only pressed the `pressed_key` key on our keyboard,
                 // otherwise all entities would move, i.e: the whole keyboard.
-                if name.as_str() == released_key.unwrap().as_str() {
+                if key.name == released_key.unwrap().as_str() {
                     transform.translation.y += 0.05;
 
                     commands.spawn(AudioBundle {
