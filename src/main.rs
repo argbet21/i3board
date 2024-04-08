bfc6c7d287cd70d2494cfd7e9ca159bfb7e93659
Alireza Zabet <ahzabet23@gmail.com>
2024-04-08 23:12:14 +0400

    Update system scheduling and module organization in `main.rs`
    - Add `audio` and `parser` modules to the project structure, enhancing audio handling and entity parsing capabilities.
    
    - Incorporate `mark_entities` system from `parser::parser` into the update loop. This system tags entities with the `Key` component based on predefined criteria, essential for keyboard interaction logic.
    
    - Adjust system scheduling to include `mark_entities` alongside `handle_keyboard_event` in the `Update` stage. Document the rationale for this scheduling choice, noting the necessity due to asset initialization timing in Bevy. Implement safeguards to ensure `mark_entities` only runs once per relevant entity, preventing unnecessary re-execution.
    
    These modifications optimize the application's initialization logic and runtime efficiency, addressing challenges related to asset loading and entity marking.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
+mod audio;
 mod input;
+mod parser;
 mod setup;
 
 use bevy::prelude::*;
 use input::keyboard_input::handle_keyboard_event;
+use parser::parser::mark_entities;
 use setup::setup::{spawn_camera, spawn_keyboard, spawn_light};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(ClearColor(Color::rgb(0.46, 0.46, 0.46))) // To have the canvas blend with our terminal
+        .insert_resource(ClearColor(Color::rgb(0.46, 0.46, 0.46)))
         .add_systems(Startup, (spawn_camera, spawn_light, spawn_keyboard))
-        .add_systems(Update, handle_keyboard_event)
+        // `mark_entities` should ideally be scheduled at `Startup`.
+        // However, after testing and observation, this isn't possible as it seems that Bevy runs the `mark_entities` system *before* all our Blender assets have fully initialized.
+        // Thus, a one-time run of `mark_entities` at `Startup` before asset initialization results in no entity marking occurring (which other systems *rely* on).
+        // In order to combat this effect, we have `mark_entities` under the `Update` schedule and have mechanisms in-place (in its implementation) to *not* have the system constantly mark entities every frame.
+        // See `crate::parser::parser::mark_entities` for more details.
+        .add_systems(Update, (mark_entities, handle_keyboard_event).chain())
         .run();
 }
